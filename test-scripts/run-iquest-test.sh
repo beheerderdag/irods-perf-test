3642bec4443824a24f33de400a9d895c716bd089
Sharif Islam <sharif.islam@surfsara.nl>
2017-11-23 16:35:33 +0100

    Update run-iquest-test.sh

--- test-scripts/run-iquest-test.sh
+++ test-scripts/run-iquest-test.sh
@@ -17,19 +17,19 @@ iquest "select count(RESC_NAME)" 1>>$outputfilename
 
 { date &  echo "== Q5 order by create time ==" ; }  >> $outputfilename ; /usr/bin/time -f "%e" iquest --no-page "SELECT DATA_NAME, COLL_NAME, DATA_REPL_NUM, DATA_SIZE, order(DATA_CREATE_TIME)" 2>> $outputfilename
 
-{ date &  echo "== Q6 data path query with count ==" ;  }  >>$outputfilename ;  /usr/bin/time -f "%e" iquest --no-page "SELECT count(DATA_NAME)  WHERE DATA_PATH like  '/eudatCache/Vault/PerfResc/home/perf/200k%'" 2>> $outputfilename
+{ date &  echo "== Q6 data path query with count ==" ;  }  >>$outputfilename ;  /usr/bin/time -f "%e" iquest --no-page "SELECT count(DATA_NAME)  WHERE DATA_PATH like  '%test%'" 2>> $outputfilename
 
 { date &  echo "== Q7 data path query with details ==" ;} >> $outputfilename ; /usr/bin/time -f "%e" iquest --no-page "SELECT DATA_NAME, DATA_CHECKSUM, DATA_VERSION, DATA_PATH, DATA_COLL_ID, DATA_RESC_NAME, DATA_STATUS  WHERE DATA_PATH like '%file.1%'" 2>>$outputfilename 
 
 { date &  echo "== Q8 list all meta data ==" ;          } >> $outputfilename ; /usr/bin/time -f "%e"   iquest --no-page "SELECT META_DATA_ATTR_NAME" 2>>$outputfilename 
 
-{ date &  echo "== Q9 meta data filter by value ==" ;   } >> $outputfilename ; /usr/bin/time -f "%e"   iquest --no-page "SELECT META_DATA_ATTR_NAME , META_DATA_ATTR_VALUE where META_DATA_ATTR_VALUE like '%21.%'" 2>> $outputfilename 
+{ date &  echo "== Q9 meta data filter by value ==" ;   } >> $outputfilename ; /usr/bin/time -f "%e"   iquest --no-page "SELECT META_DATA_ATTR_NAME , META_DATA_ATTR_VALUE where META_DATA_ATTR_VALUE like '%blue.%'" 2>> $outputfilename 
 
 { date &  echo "== Q10 data path filter by value ==";   } >> $outputfilename ; /usr/bin/time -f "%e"   iquest --no-page "SELECT DATA_PATH WHERE DATA_PATH like '%9999%' || like '%2%'" 2>>$outputfilename 
 
-{ date &  echo "== Q11 resource name and meta data look up ==" ; } >> $outputfilename ; /usr/bin/time -f "%e"  iquest --no-page "select COLL_NAME,DATA_NAME where COLL_NAME like '%perf%' AND RESC_NAME = 'PerfResc' and AND META_DATA_ATTR_VALUE like '2%'" 2>>$outputfilename 
+{ date &  echo "== Q11 resource name and meta data look up ==" ; } >> $outputfilename ; /usr/bin/time -f "%e"  iquest --no-page "select COLL_NAME,DATA_NAME where COLL_NAME like '%perf%' AND RESC_NAME = 'MyResc' and AND META_DATA_ATTR_VALUE like '2%'" 2>>$outputfilename 
 
-{ date &  echo "== Q12 OR filter meta data value =="    ;        } >> $outputfilename ; /usr/bin/time -f "%e"  iquest --no-page "SELECT META_DATA_ATTR_NAME , META_DATA_ATTR_VALUE where META_DATA_ATTR_VALUE like '%21.%' || like 'True'" 2>>$outputfilename
+{ date &  echo "== Q12 OR filter meta data value =="    ;        } >> $outputfilename ; /usr/bin/time -f "%e"  iquest --no-page "SELECT META_DATA_ATTR_NAME , META_DATA_ATTR_VALUE where META_DATA_ATTR_VALUE like '%blue.%' || like 'green'" 2>>$outputfilename
 
 { date &  echo "== Q13 COLL name filter ==";                     } >> $outputfilename ; /usr/bin/time -f "%e"  iquest --no-page "SELECT COLL_NAME, META_COLL_ATTR_VALUE WHERE COLL_NAME like '%perf%'" 2>>$outputfilename
 
